a086a0a71bca0b3702423ce7503f668b11ae5704
Rania Oueslati <110637481+raniaoues@users.noreply.github.com>
2026-05-01 02:50:40 +0100

    Update user.service.ts

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -13,7 +13,7 @@ export interface User {
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/api/users';
+  private apiUrl = 'http://projet-cloud-alb-695171246.us-east-1.elb.amazonaws.com/api/users';
 
   constructor(private http: HttpClient) { }
 
